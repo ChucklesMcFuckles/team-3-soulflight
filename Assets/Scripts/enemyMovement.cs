c5b9231c62883f451074852d028d36f1a353f133
ChucklesMcFuckles <jameshall620@gmail.com>
2023-06-03 18:59:51 -0700

    First Commit
    Hopefully this works, and the entire staging folder is committed.

---
+++ Assets/Scripts/enemyMovement.cs
@@ -0,0 +1,125 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class enemyMovement : MonoBehaviour
+{
+    public Transform player;
+    public NavMeshAgent agent;
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    //Patroling
+    public Vector3 walkPoint;
+    public bool walkPointSet;
+    public float walkPointRange = 2;
+
+    //Attacking
+    public float timeBetweenAttacks;
+    public bool alreadyAttacked;
+    public GameObject projectile;
+
+    //States
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    public void Start()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    public void Update()
+    {
+        //check if Player is in range
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if(!playerInAttackRange && !playerInSightRange) Patroling();
+        else if(playerInSightRange && !playerInAttackRange) ChasePlayer();
+        else if(playerInSightRange && playerInAttackRange) AttackPlayer();
+
+        /*transform.LookAt(player);
+
+        agent.SetDestination(player.transform.position);
+
+        if (Vector3.Distance(transform.position, player.position) < enemyDistance)
+        {
+	    //Reaches the player
+            gameObject.GetComponent<NavMeshAgent>().velocity = Vector3.zero;
+        }*/
+    }
+
+    private void Patroling()
+    {
+        //Make a walkPointSet variable
+        if(!walkPointSet) SearchWalkPoint();
+
+        if(walkPointSet)
+            agent.SetDestination(walkPoint);
+        
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        //Walkpoint reached
+        if(distanceToWalkPoint.magnitude < 1f)
+            walkPointSet = false;
+    }
+
+    private void SearchWalkPoint()
+    {
+        //Calculate random point in range
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+        if(Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+            walkPointSet = true;
+    }
+
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+
+    private void AttackPlayer()
+    {
+        //Make sure enemy doesn't move
+        agent.SetDestination(transform.position);
+        transform.LookAt(player);
+
+        if(!alreadyAttacked)
+        {
+            //Attack code here (This is a placeholder!)
+            
+            Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+            rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
+            rb.AddForce(transform.up * 8f, ForceMode.Impulse);
+            //
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+
+    //Visualizes the enemy attack
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+    }
+
+    /*public float enemyDistance = 0.7f;
+    private void Start()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+
+        agent = GetComponent<NavMeshAgent>();
+    }*/
+
+}
