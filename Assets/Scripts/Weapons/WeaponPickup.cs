9a9cb876f1f236cbd15b609fe2d3af7cfe64afbe
James <jameshall620@gmail.com>
2023-06-06 13:53:06 -0700

    update

---
+++ Assets/Scripts/Weapons/WeaponPickup.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponPickup : MonoBehaviour
+{
+    public Transform equipPosition;
+    public float distance = 10f;
+    GameObject currentWeapon;
+
+    //wp was set to static to help fix the issue of the weapons previously dropped staying with player in new scenes.
+    //Though this fix is not very effective as those objects will continue to exist and slow down the game.
+    static GameObject wp;
+
+    private bool canGrab;
+
+    void Update()
+    {
+        CheckWeapons();
+        if(canGrab)
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                if(currentWeapon != null)
+                    Drop();
+                PickUp();
+            }
+        }
+
+        if(currentWeapon != null)
+        {
+            if(Input.GetKeyDown(KeyCode.Q))
+                Drop();
+        }
+    }
+
+    private void CheckWeapons()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, distance))
+        {
+            if(hit.transform.tag == "CanGrab")
+            {
+                Debug.Log("I can grab it");
+                canGrab = true;
+                wp = hit.transform.gameObject;
+            }
+        }
+
+        else
+            canGrab = false;
+    }
+
+    private void PickUp()
+    {
+        currentWeapon = wp;
+        currentWeapon.transform.position = equipPosition.position;
+        currentWeapon.transform.parent = equipPosition;
+        currentWeapon.transform.localEulerAngles = new Vector3(0f, 180f, 0f);
+        currentWeapon.GetComponent<Rigidbody>().isKinematic = true;
+        currentWeapon.GetComponent<Collider>().isTrigger = true;
+    }
+
+    private void Drop()
+    {
+        currentWeapon.transform.parent = null;
+        currentWeapon.GetComponent<Rigidbody>().isKinematic = false;
+        currentWeapon.GetComponent<Collider>().isTrigger = false;
+        currentWeapon = null;
+    }
+}
